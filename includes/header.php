02f4a2bc0087f321c917efd8ba50f10a5a5806e1
Leo Severo <leocsevero@gmail.com>
2024-09-13 19:08:41 -0300

    1.3 Adicionado  Nome e foto na barra esquerda do projeto

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,6 @@ if (!isset($_SESSION['loginUser']) && !isset($_SESSION['senhaUser'])) {
     header("Location: ../index.php?acao=negado");
     exit;
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -51,78 +50,107 @@ if (!isset($_SESSION['loginUser']) && !isset($_SESSION['senhaUser'])) {
             <img class="animation__shake" src="../dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60"
                 width="60" />
         </div>
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-            </ul>
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Notifications Dropdown Menu -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#" title="Perfil e Sair">
-                        <i class="fa fa-user-circle" aria-hidden="true"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <div class="dropdown-divider"></div>
-                        <a href="home.php?acao=perfil" class="dropdown-item">
-                            <i class="fa fa-user-circle mr-2"></i> Alterar Perfil
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="../config/logout.php" class="dropdown-item">
-                            <i class="fa fa-sign-out-alt mr-2" aria-hidden="true"></i> Sair
-                        </a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
+        <?php
+        try {
+            include_once('../config/conexao.php');
+            $email = $_SESSION['loginUser'];
+            $senha = $_SESSION['senhaUser'];
 
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <a href="#" class="brand-link">
-                <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: 0.8" />
-                <span class="brand-text font-weight-light">Agenda Eletronica</span>
-            </a>
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="../img/<?php print_r($_SESSION['foto_user']) ?>" class="img-circle elevation-2"
-                            alt="User Image" />
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block"><?php print_r($_SESSION['nome_user']) ?></a>
-                    </div>
-                </div>
+            $sql = "SELECT * FROM tb_user WHERE email_user=:email AND senha_user=:senha";
+            $prepare = $conexao->prepare($sql);
+            $prepare->bindValue(":email", $email, PDO::PARAM_STR);
+            $prepare->bindValue(":senha", $senha, PDO::PARAM_STR);
+            $prepare->execute();
 
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                        data-accordion="false">
-                        <!-- Add icons to the links using the .nav-icon class
+            // Verifica se há resultados
+            if ($prepare->rowCount() > 0) {
+                // Itera sobre os resultados
+                while ($row = $prepare->fetch(PDO::FETCH_OBJ)) {
+                    $nomeUser = $row->nome_user;  // Acessa o nome como propriedade do objeto
+                    $fotoUser = $row->foto_user;  // Acessa a foto como propriedade do objeto 
+        
+
+                    ?>
+                    <!-- Navbar -->
+                    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+                        <!-- Left navbar links -->
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                            </li>
+                        </ul>
+                        <!-- Right navbar links -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Notifications Dropdown Menu -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" data-toggle="dropdown" href="#" title="Perfil e Sair">
+                                    <i class="fa fa-user-circle" aria-hidden="true"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                                    <div class="dropdown-divider"></div>
+                                    <a href="home.php?acao=perfil" class="dropdown-item">
+                                        <i class="fa fa-user-circle mr-2"></i> Alterar Perfil
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a href="../config/logout.php" class="dropdown-item">
+                                        <i class="fa fa-sign-out-alt mr-2" aria-hidden="true"></i> Sair
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- /.navbar -->
+                    <!-- Main Sidebar Container -->
+                    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                        <!-- Brand Logo -->
+                        <a href="#" class="brand-link">
+                            <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                                style="opacity: 0.8" />
+                            <span class="brand-text font-weight-light">Agenda Eletronica</span>
+                        </a>
+                        <!-- Sidebar -->
+                        <div class="sidebar">
+                            <!-- Sidebar user panel (optional) -->
+                            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                                <div class="image">
+                                    <img src="../img/<?php echo $fotoUser; ?>" class="img-circle elevation-2" alt="User Image" />
+                                </div>
+                                <div class="info">
+                                    <a href="#" class="d-block"><?php echo $nomeUser; ?></a>
+                                </div>
+                            </div>
+
+                            <!-- Sidebar Menu -->
+                            <nav class="mt-2">
+                                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                                    data-accordion="false">
+                                    <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-                        <li class="nav-item">
-                            <a href="home.php?acao=bemvindo" class="nav-link">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p> Principal</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="home.php?acao=relatorio" class="nav-link">
-                                <i class="nav-icon fas fa-chart-pie"></i>
-                                <p> Relatório</p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
-            </div>
-            <!-- /.sidebar -->
+                                    <li class="nav-item">
+                                        <a href="home.php?acao=bemvindo" class="nav-link">
+                                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                                            <p> Principal</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="home.php?acao=relatorio" class="nav-link">
+                                            <i class="nav-icon fas fa-chart-pie"></i>
+                                            <p> Relatório</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <!-- /.sidebar-menu -->
+                        </div>
+                        <!-- /.sidebar -->
+                        <?php
+                }
+            } else {
+                echo "Usuário Não Logado.";
+            }
+
+        } catch (PDOException $err) {
+            echo $err->getMessage();
+        }
+        ?>
         </aside>
